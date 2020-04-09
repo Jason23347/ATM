63bfb0d60e867003c424ec3fbfc4f1b79c93ddf4
jason23347 <jason23347@gmail.com>
2020-04-09 23:45:42 +0800

    user: fixed string mistaken

--- atm/app/User.php
+++ atm/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'crad_number',
+        'card_number',
         'balance',
         'password',
     ];
